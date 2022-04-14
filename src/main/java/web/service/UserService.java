c328c636229b48932901c611d29c3ed91afa07cf
komXXme <seoprokom@gmail.com>
2022-04-14 05:26:59 +0400

    Initial commit

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,18 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public void addUser(User user);
+
+    public User getUserById(int id);
+
+    public User updateUser(User user);
+
+    public void removeUserById(int id);
+
+    public List<User> listUsers();
+}
